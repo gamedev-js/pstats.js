d0e56a8a5f66f450ff57916bb4cf77c69dfcd858
Johnny Wu <KarasAya@gmail.com>
2017-04-04 02:21:30 +0800

    remove webgl

--- README.md
+++ README.md
@@ -17,27 +17,52 @@ npm install pstats.js
 ```javascript
 let stats = pstats.new(document.body, {
   values: {
-    frame: { desc: 'Total frame time (ms)', over: 18 },
-    raf: { desc: 'Time since last rAF (ms)' },
-    fps: { desc: 'Framerate (FPS)', below: 30 },
+    frame: { desc: 'Total frame time (ms)', over: 18, average: 100 },
+    raf: { desc: 'Time since last rAF (ms)', average: 100 },
+    fps: { desc: 'Framerate (FPS)', below: 30, average: 500 },
     render1: { desc: 'WebGL Render 01 (ms)' },
     render2: { desc: 'WebGL Render 02 (ms)' },
+    user01: { desc: 'User Value 01 (ranged)', min: -1, max: 1, color: '#080' },
+    user02: { desc: 'User Value 02 (threshold)', color: '#09f', threshold: true },
+    memory: { desc: 'Memory', extension : 'memory.used', average: 1000, threshold: true },
   },
   fractions: [
-    { base: 'frame', steps: ['render1', 'render2'] }
+    { base: 'frame', steps: ['render1', 'render2'] },
+    { base: 'frame', steps: ['render1', 'render2'], colors: ['#09f', '#f90'] },
+  ],
+  extensions: [
+    'memory'
   ],
 });
 
+function delay() {
+  let d = Math.random() * 10;
+  let now = Date.now();
+  let s = now + d;
+  while (now < s) {
+    now = Date.now();
+  }
+}
+
+let t = 0;
+
 function render() {
   stats('frame').start();
+    stats('memory').snapshot();
     stats('raf').tick();
     stats('fps').frame();
 
     stats('render1').start();
+      delay();
     stats('render1').end();
 
     stats('render2').start();
+      delay();
     stats('render2').end();
+
+    stats('user01').value = Math.sin(t);
+    stats('user02').value = Math.pow(t % 10, 3);
+    t += 0.1;
   stats('frame').end();
 
   stats().tick();
--- dist/pstats.js
+++ dist/pstats.js
@@ -392,9 +392,8 @@ class MemoryStats {
   }
 
   tick () {
-    let cur = window.performance.memory.usedJSHeapSize;
     this._lastUsed = this._used;
-    this._used = cur;
+    this._used = window.performance.memory.usedJSHeapSize;
     this._total = window.performance.memory.totalJSHeapSize;
   }
 
@@ -404,7 +403,7 @@ class MemoryStats {
   }
 
   get used() {
-    return this._used;
+    return window.performance.memory.usedJSHeapSize;
   }
 
   get total() {
@@ -416,139 +415,8 @@ class MemoryStats {
   }
 }
 
-let _hacked = false;
-let _totalDrawArraysCalls = 0;
-let _totalDrawElementsCalls = 0;
-let _totalUseProgramCalls = 0;
-let _totalFaces = 0;
-let _totalVertices = 0;
-let _totalPoints = 0;
-let _totalBindTexures = 0;
-
-function _hackWebGL() {
-  if (_hacked) {
-    return;
-  }
-
-  _hacked = true;
-
-  function _h(f, c) {
-    return function () {
-      c.apply(this, arguments);
-      f.apply(this, arguments);
-    };
-  }
-
-  WebGLRenderingContext.prototype.drawArrays = _h(WebGLRenderingContext.prototype.drawArrays, function () {
-    _totalDrawArraysCalls++;
-    if (arguments[0] == this.POINTS) _totalPoints += arguments[2];
-    else _totalVertices += arguments[2];
-  });
-
-  WebGLRenderingContext.prototype.drawElements = _h(WebGLRenderingContext.prototype.drawElements, function () {
-    _totalDrawElementsCalls++;
-    _totalFaces += arguments[1] / 3;
-    _totalVertices += arguments[1];
-  });
-
-  WebGLRenderingContext.prototype.useProgram = _h(WebGLRenderingContext.prototype.useProgram, function () {
-    _totalUseProgramCalls++;
-  });
-
-  WebGLRenderingContext.prototype.bindTexture = _h(WebGLRenderingContext.prototype.bindTexture, function () {
-    _totalBindTexures++;
-  });
-
-  // WebGLRenderingContext.prototype.flush = _h(WebGLRenderingContext.prototype.flush, function () {
-  //   _totalDrawArraysCalls = 0;
-  //   _totalDrawElementsCalls = 0;
-  //   _totalUseProgramCalls = 0;
-  //   _totalFaces = 0;
-  //   _totalVertices = 0;
-  //   _totalPoints = 0;
-  //   _totalBindTexures = 0;
-  // });
-}
-
-class WebglCounter extends Counter {
-  constructor(stats, id, opts) {
-    super(id, opts);
-    this._stats = stats;
-    this._start = 0;
-
-    if ( opts.extension.indexOf('webgl.') === 0 ) {
-      this._field = opts.extension.substring(6);
-    }
-  }
-
-  snapshot () {
-    this._value = this._stats[this._field];
-  }
-
-  start() {
-    this._start = this._stats[this._field];
-  }
-
-  end() {
-    this._value = this._stats[this._field] - this._start;
-  }
-}
-
-class WebGLStats {
-  constructor () {
-    _hackWebGL();
-  }
-
-  tick () {
-    _totalDrawArraysCalls = 0;
-    _totalDrawElementsCalls = 0;
-    _totalUseProgramCalls = 0;
-    _totalFaces = 0;
-    _totalVertices = 0;
-    _totalPoints = 0;
-    _totalBindTexures = 0;
-  }
-
-  counter(id, opts) {
-    return new WebglCounter(this, id, opts);
-  }
-
-  get drawcalls () {
-    return _totalDrawArraysCalls + _totalDrawElementsCalls;
-  }
-
-  get drawArrays () {
-    return _totalDrawArraysCalls;
-  }
-
-  get drawElements () {
-    return _totalDrawElementsCalls;
-  }
-
-  get faces () {
-    return _totalFaces;
-  }
-
-  get vertices () {
-    return _totalVertices;
-  }
-
-  get points () {
-    return _totalPoints;
-  }
-
-  get programs () {
-    return _totalUseProgramCalls;
-  }
-
-  get textures () {
-    return _totalBindTexures;
-  }
-}
-
 let extensions = {
   memory: MemoryStats,
-  webgl: WebGLStats,
 };
 
 let _canvasWidth = 100;
